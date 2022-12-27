38d2cdc96a8e87061bca421a90a774b12d3f479a
unknown <saritadh74@gmail.com>
2022-12-27 04:06:40 +0200

    finished setting up the project relates #1

---
+++ src/interfaces/IRequestsApi.ts
@@ -0,0 +1,10 @@
+export default interface IRequestApi {
+    topRated: string,
+    trending: string,
+    comedyMovies: string,
+    actionMovies: string,
+    horrorMovies: string,
+    romanceMovies: string,
+    documentaries: string,
+    netflixOriginals: string
+}
