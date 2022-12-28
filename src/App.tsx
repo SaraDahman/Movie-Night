a02b683276cb003fe3560faf83c20d58c1907f16
unknown <saritadh74@gmail.com>
2022-12-28 21:20:15 +0200

    finished styling the header section and modified the style of the row component relates #3

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './style/App.css'
 
 function App() {  
   return (
-    <div className="App">
+    <div className="app">
       <Header />
       {requests.map((request, i) => <Row key={i} 
       title ={request.title} 
