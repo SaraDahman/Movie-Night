a469631168885f6695bc90a36311242162a36b0e
unknown <saritadh74@gmail.com>
2022-12-30 21:50:09 +0200

    deploying the clone

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,42 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { FC, useEffect, useState } from 'react'
+import movieTrailer from 'movie-trailer'
+import YouTube from 'react-youtube'
 import requests from '../requests'
 import axios from '../axios'
 import { IMovie } from '../interfaces'
 import '../style/Header.css'
 
 const Header:FC = () =>{
-  const [movie, setMovie] = useState<IMovie | null>(null)
-  const imageBaseUrl:string = 'https://image.tmdb.org/t/p/original'
+  const [movie, setMovie] = useState<IMovie | null>(null);
+  const imageBaseUrl:string = 'https://image.tmdb.org/t/p/original';
 
+  // const [movieUrl, setMovieUrl] = useState<string | null>(null);
+  //   const opts = {
+  //     playerVars: {
+  //       autoplay: 1,
+  //       loop: 1,
+  //       controls: 0,
+  //     },
+  //   }
   useEffect(()=>{
     const fetchData = async () =>{
         try {
             const {data} = await axios.get(requests[1].fetchUrl)
             const random = Math.floor(Math.random() * data.results.length-1)
-            setMovie(data.results[random]);            
+
+            const mov = data.results[random]
+            setMovie(mov);
+            // const url = await movieTrailer(mov.name || mov.title || mov.original_title)  
+            
+            // console.log(mov);
+
+            // const urlParams = new URLSearchParams(new URL(url).search)
+            // const videoId = urlParams.get('v')
+
+            // setMovieUrl(videoId)
+            // console.log(url, typeof videoId);
+            
           } catch (error) {
             console.log(error, 'error in fetching header data');   
           }
@@ -52,6 +74,7 @@ const Header:FC = () =>{
           {truncate(movie?.overview, 50)}
         </p>
       </div>
+      {/* <YouTube videoId={movieUrl } opts={opts} className='video'/> */}
     </header>
   )
 }
